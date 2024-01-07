ae0bfb6e2765abc4eb62ec0679bae50a8e39a79e
xiaojinli <xiaojin20135@live.com>
2024-01-07 15:55:33 +0800

    feat:顶部导航栏

--- app/src/main/java/com/example/mukepro/TopTabActivity.kt
+++ app/src/main/java/com/example/mukepro/TopTabActivity.kt
@@ -1,11 +1,46 @@
 package com.example.mukepro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.library_ui.tab.common.IHiTabLayout
+import com.example.library_ui.tab.top.HiTabTopInfo
+import com.example.library_ui.tab.top.HiTabTopLayout
 
 class TopTabActivity : AppCompatActivity() {
+    var tabsStr = arrayOf(
+        "热门",
+        "服装",
+        "数码",
+        "鞋子",
+        "零食",
+        "家电",
+        "汽车",
+        "百货",
+        "家居",
+        "装修",
+        "运动"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_tab)
+        initTabTop()
+    }
+
+    private fun initTabTop() {
+        val hiTabTopLayout: HiTabTopLayout = findViewById(R.id.tab_top_layout)
+        val infoList: MutableList<HiTabTopInfo<*>> = ArrayList<HiTabTopInfo<*>>()
+        val defaultColor = resources.getColor(R.color.tabBottomDefaultColor)
+        val tintColor = resources.getColor(R.color.tabBottomTintColor)
+        for (s in tabsStr) {
+            val info: HiTabTopInfo<*> = HiTabTopInfo<Int>(s, defaultColor, tintColor)
+            infoList.add(info)
+        }
+        hiTabTopLayout.inflateInfo(infoList)
+        hiTabTopLayout.addTabSelectedChangeLisntener { index, preInfo, nextInfo ->
+            Toast.makeText(this@TopTabActivity, nextInfo.name, Toast.LENGTH_SHORT).show()
+        }
+        hiTabTopLayout.defaultSelected(infoList[0])
     }
 }
